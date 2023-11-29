9f29292d9a48c0056a03f0b940bd76d97f38cc7a
yooInTheBuilding <dbals9926@gmail.com>
2023-11-29 15:39:36 +0900

    first commit

---
+++ src/a/First.java
@@ -0,0 +1,15 @@
+//현재 클래스가 소속된 패키지의 이름
+package a;
+
+//현재 클래스의 이름
+public class First { //first class 블록의 시작점
+	
+	// main method(프로그램의 시작지점)
+	public static void main(String[] args) { //main method 블록의 시작
+		// 실행할 내용이 작성되는 공간
+		// 텍스트 또는 변수값을 출력할 수 있는 println() method
+		System.out.println("이거 겁나 불편하네");
+		
+	}// main method 블록의 끝
+
+} // first class 블록의 끝
---
+++ src/b/Second.java
@@ -0,0 +1,9 @@
+package b;
+
+public class Second {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!");
+	}
+
+}
